500b8ef034800fa5170fe3a3c02b2c678f6fdfda
yaderxd <yaderoviedo4@gmail.com>
2022-05-12 10:22:22 -0600

    cambio

---
+++ DepreciationDBApp.Domain/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using DepreciationDBApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepreciationDBApp.Domain.Interfaces
+{
+    public interface IEmployeeRepository:IRepository<Employee>
+    {
+        Employee FIndByDni(string dni);
+        Employee FindByEmail(string email);
+        IEnumerable<Employee> FindByLastNames(string lastnames);
+    }
+}
