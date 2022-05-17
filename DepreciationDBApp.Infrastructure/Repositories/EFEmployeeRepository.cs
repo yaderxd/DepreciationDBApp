985e115158a890b02ffae37c469b1ee18cbcc493
yaderxd <yaderoviedo4@gmail.com>
2022-05-17 09:54:07 -0600

    cambio

--- DepreciationDBApp.Infrastructure/Repositories/EFEmployeeRepository.cs
+++ DepreciationDBApp.Infrastructure/Repositories/EFEmployeeRepository.cs
@@ -10,6 +10,7 @@ namespace DepreciationDBApp.Infrastructure.Repositories
 {
     public class EFEmployeeRepository : IEmployeeRepository
     {
+        public IDepreciationDbContext depreciationDbContext;
         public void Create(Employee t)
         {
             throw new NotImplementedException();
@@ -17,12 +18,41 @@ namespace DepreciationDBApp.Infrastructure.Repositories
 
         public bool Delete(Employee t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (t == null)
+                {
+                    throw new ArgumentException("El Objeto Asset no puede ser null");
+                }
+                Employee employee = FIndByDni(t.Dni);
+                if (employee == null)
+                {
+                    throw new Exception($"El Objeto con Id {t.Dni} no existe");
+                }
+                depreciationDbContext.Employees.Remove(employee);
+                int result = depreciationDbContext.SaveChanges();
+                return result > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public Employee FIndByDni(string dni)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(dni))
+                {
+                    throw new ArgumentException($"");
+                }
+                return depreciationDbContext.Employees.FirstOrDefault(x => x.Dni.Equals(dni));
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public Employee FindByEmail(string email)
@@ -32,22 +62,70 @@ namespace DepreciationDBApp.Infrastructure.Repositories
 
         public IEnumerable<Employee> FindByLastNames(string lastnames)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (string.IsNullOrWhiteSpace(lastnames))
+                {
+                    throw new ArgumentException($"");
+                }
+                return depreciationDbContext.Employees.Where(x => x.Lastnames.Equals(lastnames, StringComparison.CurrentCultureIgnoreCase)).ToList();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public List<Employee> GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return depreciationDbContext.Employees.ToList();
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public int Update(Employee t)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (t == null)
+                {
+                    throw new ArgumentNullException($"El Objeto no se puede ser null");
+                }
+                Employee employee = FIndByDni(t.Dni);
+                if (employee == null)
+                {
+                    throw new Exception($"El objeto asset con id {t.Dni} no existe");
+                }
+                employee.Names = t.Names;
+                employee.Lastnames = t.Lastnames;
+                employee.Adress = t.Adress;
+                //asset.AmountResidual = t.AmountResidual;
+                employee.Phone = t.Phone;
+                employee.Email = t.Email;
+                employee.Status = t.Status;
+                //asset.Code = t.Code;
+
+                depreciationDbContext.Employees.Update(employee);
+                return depreciationDbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
         }
 
         public int xd(Employee t)
         {
             throw new NotImplementedException();
+        }
+        public void validateEmployee()
+        {
+
         }
     }
 }
