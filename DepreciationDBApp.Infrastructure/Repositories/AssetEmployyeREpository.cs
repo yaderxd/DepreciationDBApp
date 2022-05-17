1616018e4eb2a46910aa3e0d4e4f63575188a436
yaderxd <yaderoviedo4@gmail.com>
2022-05-17 10:12:00 -0600

    cambio

---
+++ DepreciationDBApp.Infrastructure/Repositories/AssetEmployyeREpository.cs
@@ -0,0 +1,49 @@
+﻿using DepreciationDBApp.Domain.Entities;
+using DepreciationDBApp.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepreciationDBApp.Infrastructure.Repositories
+{
+    public class AssetEmployyeREpository : IAssetEmployeeRepository
+    {
+        public IDepreciationDbContext depreciationDbContext;
+        public void Create(AssetEmployee t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(AssetEmployee t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AssetEmployee> FindByAssetId(int assetId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AssetEmployee> FindByEmployeeId(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AssetEmployee> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(AssetEmployee t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int xd(AssetEmployee t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
